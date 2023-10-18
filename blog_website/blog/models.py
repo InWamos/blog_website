2113f0df1bc616608f46e99e330366bc94034d2d
W <mykyta.pidgornyi@gmail.com>
2023-10-18 19:19:07 +0300

    basic post model

--- blog_website/blog/models.py
+++ blog_website/blog/models.py
@@ -1,3 +1,16 @@
+from django.utils import timezone
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
