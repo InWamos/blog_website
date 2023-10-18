aa0fca233f8deeb1df7d92cc7c5fe71758ba4818
W <mykyta.pidgornyi@gmail.com>
2023-10-18 21:41:42 +0300

    added Status subclass to Post model

--- blog_website/blog/models.py
+++ blog_website/blog/models.py
@@ -5,12 +5,22 @@ from django.db import models
 
 
 class Post(models.Model):
+
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PD', 'Published'
+
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=2,
+        choices=Status.choices,
+        default=Status.DRAFT,
+    )
 
     class Meta:
         ordering = ['-publish']
