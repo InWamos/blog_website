c9dd1887e4b8223d611417b4c0ff860fa4c28e6a
W <mykyta.pidgornyi@gmail.com>
2023-10-19 10:17:00 +0300

    Author column as a foreign key

--- blog_website/blog/models.py
+++ blog_website/blog/models.py
@@ -1,6 +1,6 @@
 from django.utils import timezone
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
@@ -13,6 +13,11 @@ class Post(models.Model):
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
+    author = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        related_name='blog_posts'
+    )
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
