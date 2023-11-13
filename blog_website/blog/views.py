3213d7971069c492febc479a8124095826db4c44
W <mykyta.pidgornyi@gmail.com>
2023-11-13 22:24:40 +0200

    post_list func to get all published posts

--- blog_website/blog/views.py
+++ blog_website/blog/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Post
+
+
+def post_list(request) -> HttpResponse:
+    posts = Post.published.all() # type: ignore
+    return render(request, "blog/post.list.html", {"posts": posts})
