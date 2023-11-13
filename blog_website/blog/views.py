64210cdc4d99402e0674cca485bc5bcbbaf2f965
InWamos <mykyta.pidgornyi@gmail.com>
2023-11-13 22:59:32 +0200

    get post details function

--- blog_website/blog/views.py
+++ blog_website/blog/views.py
@@ -1,10 +1,23 @@
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse, Http404
 from django.shortcuts import render
 
 # Create your views here.
 from .models import Post
 
 
-def post_list(request) -> HttpResponse:
-    posts = Post.published.all() # type: ignore
-    return render(request, "blog/post.list.html", {"posts": posts})
+def post_list(request: HttpRequest) -> HttpResponse:
+    posts = Post.published.all()
+    return render(
+        request=request, template_name="blog/post.list.html", context={"posts": posts}
+    )
+
+
+def post_detail(request: HttpRequest, id: int) -> HttpResponse:
+    try:
+        post = Post.published.get(id=id)
+    except Post.DoesNotExist:
+        raise Http404("404 No post found.")
+
+    return render(
+        request=request, template_name="blog/post/detail.html", context={"post": post}
+    )
