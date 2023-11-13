beea4cdc35596ee7e26c5b53de1d599b2d0a0cbb
W <mykyta.pidgornyi@gmail.com>
2023-11-13 22:19:29 +0200

    Formatted models.py

--- blog_website/blog/models.py
+++ blog_website/blog/models.py
@@ -1,22 +1,20 @@
 from django.utils import timezone
 from django.db import models
 from django.contrib.auth.models import User
+
 # Create your models here.
 
 
 class Post(models.Model):
-
     class Status(models.TextChoices):
-        DRAFT = 'DF', 'Draft'
-        PUBLISHED = 'PD', 'Published'
+        DRAFT = "DF", "Draft"
+        PUBLISHED = "PD", "Published"
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
     author = models.ForeignKey(
-        to=User,
-        on_delete=models.CASCADE,
-        related_name='blog_posts'
+        to=User, on_delete=models.CASCADE, related_name="blog_posts"
     )
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
@@ -28,9 +26,10 @@ class Post(models.Model):
     )
 
     class Meta:
-        ordering = ['-publish']
+        ordering = ["-publish"]
         indexes = [
-            models.Index(fields=['-publish']),
+            models.Index(fields=["-publish"]),
         ]
+
     def __str__(self) -> str:
         return self.title
