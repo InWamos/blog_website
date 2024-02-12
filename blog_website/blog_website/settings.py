58595ca624c4bfffc4de1953a5b20a47c6ab3f06
InWamos <mykyta.pidgornyi@gmail.com>
2024-02-12 16:38:55 +0200

    connect postgres database

--- blog_website/blog_website/settings.py
+++ blog_website/blog_website/settings.py
@@ -79,13 +79,8 @@ WSGI_APPLICATION = 'blog_website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+DATABASES = db_config.POSTGRES_CONFIG.copy()
 
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
